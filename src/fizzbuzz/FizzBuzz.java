df2205a90acbd7feb3f9e853afb6ccc564fb0051
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-20 19:53:31 +0000

    fizzbuzz created

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -6,4 +6,19 @@ public class FizzBuzz {
 		return i % 3 == 0;
 	}
 
+	public Boolean isDivisibleByFive(int i) {
+		return i % 5 == 0;
+	}
+
+	public Boolean isDivisibleByFifteen(int i) {
+		return i % 15 == 0;
+	}
+
+	public String fizzBuzz(int i) {
+		if (this.isDivisibleByFifteen(i)) return "FizzBuzz";
+		if (this.isDivisibleByThree(i)) return "Fizz";
+		if (this.isDivisibleByFive(i)) return "Buzz";
+		return Integer.toString	(i);
+	}
+
 }
--- src/fizzbuzz/fizzBuzzTest.java
+++ src/fizzbuzz/fizzBuzzTest.java
@@ -13,9 +13,59 @@ public class fizzBuzzTest {
 		assertEquals(true, output);
 	}
 	
+	@Test
 	public void itKnowsOneIsNotDivisibleByThree() {
 		FizzBuzz test = new FizzBuzz();
 		Boolean output = test.isDivisibleByThree(1);
+		assertEquals(false, output);
+	}
+	
+	@Test
+	public void itKnowsFiveIsDivisibleByFive() {
+		FizzBuzz test = new FizzBuzz();
+		Boolean output = test.isDivisibleByFive(5);
 		assertEquals(true, output);
+	}
+	
+	@Test
+	public void itKnowsOneIsNotDivisibleByFive() {
+		FizzBuzz test = new FizzBuzz();
+		Boolean output = test.isDivisibleByFive(1);
+		assertEquals(false, output);
+	}
+	
+	@Test
+	public void itKnowsFifteenIsDivisibleByFifteen() {
+		FizzBuzz test = new FizzBuzz();
+		Boolean output = test.isDivisibleByFifteen(15);
+		assertEquals(true, output);
+	}
+	
+	@Test
+	public void itKnowsOneIsNotDivisibleByFifteen() {
+		FizzBuzz test = new FizzBuzz();
+		Boolean output = test.isDivisibleByFifteen(1);
+		assertEquals(false, output);
+	}
+	
+	@Test
+	public void itReturnsFizzForTheNumberThree() {
+		FizzBuzz test = new FizzBuzz();
+		String output = test.fizzBuzz(3);
+		assertEquals("Fizz", output);
+	}
+	
+	@Test
+	public void itReturnsBuzzForTheNumberFive() {
+		FizzBuzz test = new FizzBuzz();
+		String output = test.fizzBuzz(5);
+		assertEquals("Buzz", output);
+	}
+	
+	@Test
+	public void itReturnsFizzBuzzForTheNumberFifteen() {
+		FizzBuzz test = new FizzBuzz();
+		String output = test.fizzBuzz(15);
+		assertEquals("FizzBuzz", output);
 	}
 }
